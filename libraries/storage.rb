9aff56dfd9acb0f259a03c39b38775c787cc16ca
Eric Herot <eric.github@herot.com>
2014-06-26 18:04:56 -0400

    Break ruby code out into functions

---
+++ libraries/storage.rb
@@ -0,0 +1,48 @@
+module EverTools
+  class Storage
+    def dev_names
+      names = []
+      if @node['ec2'] &&
+        @node['ec2']['block_device_mapping_ephemeral1']
+        names = ec2_dev_names
+      elsif @node['etc']['passwd']['vagrant']
+        names = vagrant_dev_names
+      end
+      Chef::Log.debug 'Discovered ephemeral devices: ' + names.join(', ')
+      names
+    end
+
+    def mnt_device
+      @node['filesystem'].find { |_k, v| v['mount'] == '/mnt' }
+    end
+
+    def initialize(node)
+      @node = node
+    end
+
+    private
+
+    def local
+      non_root_bds = @node['block_device'].select { |bd, _conf| bd != 'sda' }
+      non_root_bds.select do |_bd, bd_conf|
+        bd_conf['model'] == 'VBOX HARDDISK'
+      end
+    end
+
+    def ec2_dev_names
+      e_block_devs = @node['ec2'].select do |k, _v|
+        k =~ /^block_device_mapping_ephemeral.*/
+      end
+
+      e_block_devs.map do |_k, v|
+        "/dev/#{v.sub(/^s/, 'xv')}"
+      end
+    end
+
+    def vagrant_dev_names
+      storage.local.map do |bd, _conf|
+        "/dev/#{bd}"
+      end
+    end
+  end
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,68 +15,54 @@
 Chef::Log.debug("Storage info: #{node['storage'].inspect}")
 
 if node['storage'] == {}
+  storage = EverTools::Storage.new(node)
+
   if node['ec2'] &&
     node['ec2']['block_device_mapping_ephemeral1']
-    Chef::Log.info('Running on an ec2 instance with multiple block devices.  ' \
-      'Setting up ephemeral mounts.')
+    Chef::Log.info('Using ec2 ephemeral storage')
+
     fail 'Directory /mnt not empty' if Dir.entries('/mnt') - %w(lost+found . ..) != []
 
-    e_block_devs = node['ec2'].select do |k, _v|
-      k =~ /^block_device_mapping_ephemeral.*/
-    end
-
-    dev_names = e_block_devs.map do |_k, v|
-      "/dev/#{v.sub(/^s/, 'xv')}"
-    end
-
-    mnt_device = node['filesystem'].find { |_k, v| v['mount'] == '/mnt' }
-
-    if mnt_device.nil?
-      Chef::Log.debug 'No /mnt devices found in node[:filesystem]:'
-      Chef::Log.debug node['filesystem'].inspect
-    else
+    unless storage.mnt_device.nil?
       mount '/mnt' do
-        fstype mnt_device.last['fs_type']
-        device mnt_device.first
+        fstype storage.mnt_device.last['fs_type']
+        device storage.mnt_device.first
         action :nothing
       end.run_action(:umount)
     end
   elsif node['etc']['passwd']['vagrant']
-    Chef::Log.info 'Using Vagrant storage'
-    local_storage = node['block_device'].select { |bd, _conf| bd != 'sda' }.select do |_bd, bd_conf|
-      bd_conf['model'] == 'VBOX HARDDISK'
-    end
-    dev_names = local_storage.map do |bd, _conf|
-      "/dev/#{bd}"
-    end
-    Chef::Log.debug 'Discovered ephemeral devices: ' + dev_names.join(', ')
+    Chef::Log.info('Using Vagrant storage')
   else
-    Chef::Log.debug('Not a recognized hypervisor type.  Not mounting any volumes.')
+    Chef::Log.debug('Not a recognized hypervisor type.  Doing nothing.')
   end
 
-  node.set['storage']['ephemeral_mounts'] = dev_names.each_with_index.map do |dev_name, i|
-    mount_point = "/mnt/dev#{i}"
+  unless storage.dev_names.empty?
+    node.set['storage']['ephemeral_mounts'] = storage.dev_names.each_with_index.map do |dev_name, i|
+      mount_point = "/mnt/dev#{i}"
 
-    execute "format #{dev_name} as ext3" do
-      command "mke2fs -j -F #{dev_name} -t ext3"
-      action :nothing
-      not_if { `file -s #{dev_name}` =~ /filesystem data/ }
-    end.run_action(:run)
+      execute "format #{dev_name} as ext3" do
+        command "mke2fs -j -F #{dev_name} -t ext3"
+        action :nothing
+        not_if { `file -s #{dev_name}` =~ /filesystem data/ }
+      end.run_action(:run)
 
-    directory mount_point do
-      recursive true
-      action :nothing
-    end.run_action(:create)
+      directory mount_point do
+        recursive true
+        action :nothing
+      end.run_action(:create)
 
-    mount mount_point do
-      device dev_name
-      # fstype `file -s #{dev_name}`[/\b\w+\b filesystem data/].split.first
-      action :nothing
-    end.run_action(:mount)
+      mount mount_point do
+        device dev_name
+        # fstype `file -s #{dev_name}`[/\b\w+\b filesystem data/].split.first
+        action :nothing
+      end.run_action(:mount)
 
-    mount_point
-  end if dev_names && !dev_names.empty?
+      mount_point
+    end
+  end
 
   Chef::Log.info 'Configured these ephemeral mounts: ' +
     node['storage']['ephemeral_mounts'].inspect if node['storage']['ephemeral_mounts']
+else
+  Chef::Log.info "Storage already configured: #{node['storage'].inspect}"
 end
