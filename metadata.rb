dcd587123ad779dd2a2803825a4c2250e5700396
Eric Herot <eric.github@herot.com>
2014-07-28 17:06:58 -0400

    Bump version v2.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures storage'
 long_description 'Installs/Configures storage'
-version          '2.1.3'
+version          '2.1.4'
 
 depends 'ohai'
