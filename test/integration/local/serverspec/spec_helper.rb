e84baaf12958b01ce90f5befad91e230e4f14cd6
Jeff Byrnes <jeff@evertrue.com>
2014-12-08 14:36:41 -0500

    Update spec_helper for Server v2

--- test/integration/local/serverspec/spec_helper.rb
+++ test/integration/local/serverspec/spec_helper.rb
@@ -1,11 +1,3 @@
 require 'serverspec'
-require 'pathname'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
-end
+set :backend, :exec
