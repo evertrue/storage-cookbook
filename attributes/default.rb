699e00e8fc647ffbacbb350560bbfc41eb748ef3
Jeff Byrnes <jeff@evertrue.com>
2015-04-07 11:58:19 -0400

    Remove unused attributes for AWS credentials data bag
    No need for these attributes as of c9d0c41

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,1 @@
-default['storage']['credentials']['data_bag']      = 'secrets'
-default['storage']['credentials']['data_bag_item'] = 'aws_credentials'
 default['storage']['aws_api_user']                 = 'Storage'
