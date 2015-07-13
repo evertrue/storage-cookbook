eaad7bd1850aa5b288d4e8a27f77c2799abc8840
Eric Herot <eric.github@herot.com>
2015-07-13 15:52:07 -0400

    check instance_store_volumes? after block_device_mapping_ephemeral0 because the former throws an exception when not on ec2

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,8 +24,8 @@ if File.exist?('/proc/mounts') && File.readlines('/proc/mounts').grep(%r{/mnt/de
   Chef::Log.info '/mnt/dev0 not already mounted.  Proceeding...'
 
   if node['ec2'] &&
-    storage.instance_store_volumes? &&
-    node['ec2']['block_device_mapping_ephemeral0']
+     node['ec2']['block_device_mapping_ephemeral0'] &&
+     storage.instance_store_volumes?
 
     # Unmount anything we find mounted at '/mnt' (as long as it's empty)
 
