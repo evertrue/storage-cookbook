feeb5f52d00fd8ad483007405b4a120542ca6575
Eric Herot <eric.github@herot.com>
2016-06-08 13:13:22 -0400

    Codify chef 12.10 requirement in metadata and kitchen yaml

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.0'
 
 supports 'ubuntu', '>= 12.04'
+chef_version '~> 12.10'
 
 depends 'ohai'
 depends 'et_fog', '~> 4.0'
