e668adb9fb007c9e45b7bbd69fff6102f04da90a
Eric Herot <eric.github@herot.com>
2017-03-08 23:42:50 -0500

    Remove unnecessary loop code from ebs test

--- test/integration/ebs/serverspec/default_spec.rb
+++ test/integration/ebs/serverspec/default_spec.rb
@@ -1,19 +1,15 @@
 require 'spec_helper'
 
 describe 'LVM Pool' do
-  {
-    '/dev/xvde' => '/mnt/ebs0'
-  }.each do |device, mountpoint|
-    describe file(mountpoint) do
-      it do
-        is_expected.to be_mounted.with(
-          device: device,
-          type: 'ext3',
-          options: {
-            rw: true
-          }
-        )
-      end
+  describe file('/mnt/ebs0') do
+    it do
+      is_expected.to be_mounted.with(
+        device: '/dev/xvde',
+        type: 'ext4',
+        options: {
+          rw: true
+        }
+      )
     end
   end
 
