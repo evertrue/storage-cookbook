49085b4a2459d0f024ab3e9214b870f8459c0c91
Eric Herot <eric.github@herot.com>
2017-03-09 11:04:54 -0500

    Convert LWRP to custom resource

--- resources/format_mount.rb
+++ resources/format_mount.rb
@@ -1,14 +1,24 @@
-actions :run
 default_action :run
 
-attribute :mount_point,
-          kind_of: String,
-          name_attribute: true
-attribute :device_name,
-          kind_of: String,
-          required: true
-attribute :fs_type,
-          kind_of: String,
-          default: 'ext4'
-attribute :reserved_space,
-          default: 0
+property :mount_point,    String, name_property: true
+property :device_name,    String, required: true
+property :fs_type,        String, default: 'ext4'
+property :reserved_space, default: 0
+
+action :run do
+  execute "format #{device_name} as #{fs_type}" do
+    command "mke2fs -j -m#{reserved_space} -F #{device_name} -t #{fs_type}"
+    action :run
+    not_if { `file -s #{device_name}` =~ /filesystem data/ }
+  end
+
+  directory mount_point do
+    recursive true
+    action :create
+  end
+
+  mount mount_point do
+    device device_name
+    action %i(mount enable)
+  end
+end
