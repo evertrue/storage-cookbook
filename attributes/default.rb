0fa8e488fbc92fe413255a11949aa2a29df4ab91
Dan McGuane <daniel.mcguane@evertrue.com>
2018-10-08 13:27:04 -0400

    bugfix: override filesystem['by_mountpoint'] with values from ['filesystem2'] if necessary

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default['storage']['aws_api_user'] = 'Storage'
+
+default['filesystem']['by_mountpoint'] = node['filesystem2']['by_mountpoint'] unless default['filesystem']['by_mountpoint'] || !node['filesystem2']
+default['filesystem']['by_device'] = node['filesystem2']['by_device'] unless default['filesystem']['by_device'] || !node['filesystem2']
+default['filesystem']['by_pair'] = node['filesystem2']['by_pair'] unless default['filesystem']['by_pair'] || !node['filesystem2']
