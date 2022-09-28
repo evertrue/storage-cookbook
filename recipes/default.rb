34ae4e166fa8f0aec4d8c8dc0f2406608fbc7dd9
Doug Youch <dougyouch@gmail.com>
2022-09-28 10:25:50 -0400

    updated for when ephemeral_mounts is nil

--- recipes/default.rb
+++ recipes/default.rb
@@ -79,7 +79,7 @@ else
   mount '/mnt' do
     action :disable
     device '/dev/xvdb'
-    only_if { !node['storage']['ephemeral_mounts'].size.zero? }
+    only_if { !node['storage']['ephemeral_mounts'] || !node['storage']['ephemeral_mounts'].size.zero? }
   end
 end
 
